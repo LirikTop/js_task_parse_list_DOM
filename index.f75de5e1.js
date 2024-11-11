950e0beadd874805e361895da4d093057229d68e
LirikTop <melnicukkiril2002@gmail.com>
2024-11-11 20:27:31 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,24 @@
+"use strict";
+const ul = document.querySelector("ul");
+const list = [
+    ...ul.querySelectorAll("li")
+];
+function toNumber(str) {
+    return +str.replace(/['$']/g, "").replace(/[',']/g, ".");
+}
+function sortList() {
+    list.sort((li1, li2)=>toNumber(li2.dataset.salary) - toNumber(li1.dataset.salary));
+    for (const li of list)ul.append(li);
+}
+function getEmployees() {
+    return list.map((elem)=>({
+            name: elem.textContent,
+            position: elem.dataset.position,
+            salary: elem.dataset.salary,
+            age: elem.dataset.age
+        }));
+}
+sortList();
+getEmployees();
+
+//# sourceMappingURL=index.f75de5e1.js.map
